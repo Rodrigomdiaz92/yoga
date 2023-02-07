d44b40b667772b1d08d1c9ba6b4d3fe99be66ac4
Rodrigo Martin Diaz <rodrigo.m.diaz@hotmail.com>
2023-02-07 19:59:51 -0300

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Header } from "./Header/Header";
+import { Main } from "./Main/Main";
+import { Footer } from "./Footer/Footer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Bienvenida() {
+  const name = "rodri";
+  return (
+    <div>
+      <h1>Hola {name} soy un componente</h1>
+    </div>
+  );
+}
+
+function Pregunta() {
+  const pregunta = false;
+  /*
+  if (pregunta) {
+    return <h1>Hay pregunta</h1>;
+  } else {
+    return <h1>No hay pregunta</h1>;
+  }*/
+
+  return <h1>{pregunta ? "Hay pregunta 😀" : "No hay pregunta🤣"}</h1>;
+}
+
+function Objeto() {
+  const user = {
+    nombre: "Rodrigo Diaz",
+    edad: 30,
+  };
+
+  return (
+    <h1>
+      {" "}
+      Nombre: {user.nombre} Edad:{user.edad}
+    </h1>
+  );
+  /*return <h1>{JSON.stringify(user)}</h1>;*/
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Header />
+    <Main />
+    <Footer />
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
